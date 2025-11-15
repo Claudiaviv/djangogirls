e4f3d31781de9ffabcf35fe3a2a9793c552d3e45
Claudia <claudiavivas2025@gmail.com>
2025-11-15 01:17:37 -0500

    Configuración

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for mysite project.
 
@@ -117,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# ¡NUEVA LÍNEA! Esto le dice a Django que busque archivos estáticos también en la carpeta 'static' de la raíz.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
+
 STATIC_ROOT = BASE_DIR / 'static_root'
 
 # Default primary key field type
