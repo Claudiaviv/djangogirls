827b92ac41b21f63d5a73f454975210b9b9f457b
Claudia <claudiavivas2025@gmail.com>
2025-11-14 23:53:05 -0500

    Corregir enrutamiento principal e incluir template post_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# blog/views.py
 
-# Create your views here.
+from django.shortcuts import render
+# Importa tu modelo Post
+from .models import Post
+# Importa la función timezone para filtrar por fecha de publicación
+from django.utils import timezone 
+
+def post_list(request):
+    # Obtiene todos los posts publicados y los ordena por fecha de publicación descendente
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    
+    # Envía la lista de posts al template usando un diccionario (contexto)
+    return render(request, 'blog/post_list.html', {'posts': posts})
